d715610c6ade5e18bf4c513066097f243e5a2e85
PuChen7 <puchen8295@gmail.com>
2018-01-30 23:32:01 -0500

    user panel created

--- addmajor.php
+++ addmajor.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 <title>北京游泳馆</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="panel.css" />
+
 <style>
 table {
     border-collapse: collapse;
@@ -18,17 +20,6 @@ th, td {
 }
 
 tr:hover {background-color:#f5f5f5;}
-
-#contentbody {
-  padding: 15px;
-  height: 80%;
-  width: 80%;
-  margin-left: auto ;
-  margin-right: auto ;
-  background-color: #f3f3f3;
-  border: 1px solid black;
-
-}
 </style>
 
 </head>
@@ -85,54 +76,21 @@ tr:hover {background-color:#f5f5f5;}
 
     <p style="font-weight: bold; font-size: 15px;">
       <img src="swim.png" width="30" height="30">
-      已报项目:
+      添加或删除项目:
     </p>
 
-    <?php
-      session_start();
-      // check for database connection
-      $conn=mysqli_connect("localhost","root","root","swim_registration");
-
-      if(!$conn)
-      {
-      die("Connection failed: " . mysqli_connect_error());
-      }
-
-      $sql = "SELECT * from Sport WHERE id = any (
-        select sportID from Subject where id = '".$_SESSION["id"]."' )";
-
-      $query = mysqli_query($conn, $sql);
-
-      if($query) {
-        echo "<table>";
-        echo "<tr>
-          <th>项目</th>
-          <th>米数</th>
-          <th>日期</th>
-          <th></th>
-          </tr>";
-        while ($row = mysqli_fetch_assoc($query)){
-          echo "<tr>";
-          echo "<td>{$row["sportname"]}</td>";
-          echo "<td>{$row["meter"]} 米</td>";
-          echo "<td>{$row["date"]}</td>";
-          echo '<td><button onclick="changewindow()" class="w3-btn w3-teal">修改</button</td>';
-          echo "</tr>";
-        }
-        echo "</table>";
-      }else {
-        echo "空";
-      }
-
-    ?>
-    <script>
-      function changewindow(){
-        window.open("changewindow.html", "_blank", "width=600,height=500");
-      }
-    </script>
-    <br>
-
-    <a style="font-weight: bold; color: #008080;" href="addmajor.php">添加新项目...</a>
+    <table>
+      <tr>
+        <th>项目</th>
+        <th>米数</th>
+        <th>日期</th>
+      </tr>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+        <td></td>
+      </tr>
+    </table>
   </div>
 </body>
 </html>
